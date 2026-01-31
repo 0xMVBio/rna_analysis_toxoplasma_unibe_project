dda0ed7436be5d77b721c82d178cf22806ddb996
Marcos Valdivia <mvaldivia@login8.hpc.binf.unibe.ch>
2026-01-31 18:16:50 +0100

    Update gene plots with explicit group variable for clearer x-axis labels

--- scripts/DESeq2_step6_genes.R
+++ scripts/DESeq2_step6_genes.R
@@ -3,17 +3,20 @@
 
 # these genes are highlighted in singhania et al 2019 as key responding genes to toxoplasma infection via IFN-gamma pathway
 
+# Create explicit group variable for clearer plotting
+dds$group <- factor(paste(dds$genotype, dds$infection, sep = "_"))
+
 # Gbp2 - guanylate binding protein 2
 # one of the main IFN-gamma induced genes, helps kill intracellular parasites
-plotCounts(dds, gene = "ENSMUSG00000028270", intgroup = c("infection", "genotype"))
+plotCounts(dds, gene = "ENSMUSG00000028270", intgroup = "group")
 
 # Stat1 - signal transducer and activator of transcription 1
 # central transcription factor for both type I and II interferon signaling
-plotCounts(dds, gene = "ENSMUSG00000026104", intgroup = c("infection", "genotype"))
+plotCounts(dds, gene = "ENSMUSG00000026104", intgroup = "group")
 
 # Irgm1 - immunity related GTPase family M member 1
 # important for autophagy and killing of intracellular pathogens
-plotCounts(dds, gene = "ENSMUSG00000020009", intgroup = c("infection", "genotype"))
+plotCounts(dds, gene = "ENSMUSG00000020009", intgroup = "group")
 
 
 # check if these genes are DE in our results
